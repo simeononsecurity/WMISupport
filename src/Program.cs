52a95344f883baa4bb4e06af1f509e174b77f3b9
simeononsecurity <4913771+simeononsecurity@users.noreply.github.com>
2022-12-13 20:38:15 -0600

    Test Error Handling

--- src/Program.cs
+++ src/Program.cs
@@ -61,6 +61,15 @@ namespace Ghosts.Client.Infrastructure
 
             // output
 
+            if (session.TestConnection() == true)
+            {
+                Console.WriteLine($"Connection Test Was Successful. Continuing...");
+            }
+            else
+            {
+                Console.WriteLine($"Connection Test Failed!");
+            }
+
             var operatingSystemOutput = new OperatingSystemOutput(session);
             operatingSystemOutput.Print();
 
@@ -110,26 +119,35 @@ namespace Ghosts.Client.Infrastructure
 
         public void Print()
         {
-            // use the CimSession to create a CimInstance object representing a WMI instance
-            // in this case, we're using the Win32_OperatingSystem class to get information about the operating system
-            var cimInstance = new CimInstance(@"Win32_OperatingSystem");
-            var instance = _session.GetInstance(@"root\cimv2", cimInstance);
+            try
+            {
+                // use the CimSession to create a CimInstance object representing a WMI instance
+                // in this case, we're using the Win32_OperatingSystem class to get information about the operating system
+                var cimInstance = new CimInstance(@"Win32_OperatingSystem");
+                var instance = _session.GetInstance(@"root\cimv2", cimInstance);
 
-            // print out the instance's properties
-            Console.WriteLine(
-                "Operating System Name: {0}",
-                instance.CimInstanceProperties["Name"].Value
-            );
-            Console.WriteLine(
-                "Operating System Version: {0}",
-                instance.CimInstanceProperties["Version"].Value
-            );
-            Console.WriteLine(
-                "Operating System Install Date: {0}",
-                instance.CimInstanceProperties["InstallDate"].Value
-            );
+                // print out the instance's properties
+                Console.WriteLine(
+                    "Operating System Name: {0}",
+                    instance.CimInstanceProperties["Name"].Value
+                );
+                Console.WriteLine(
+                    "Operating System Version: {0}",
+                    instance.CimInstanceProperties["Version"].Value
+                );
+                Console.WriteLine(
+                    "Operating System Install Date: {0}",
+                    instance.CimInstanceProperties["InstallDate"].Value
+                );
+            }
+            catch (CimException ex)
+            {
+                // handle any errors that occur when querying the remote computer
+                Console.WriteLine("An error occurred while querying the remote computer: {0}", ex.Message);
+            }
         }
     }
+
 
     public class BiosOutput
     {
@@ -142,23 +160,32 @@ namespace Ghosts.Client.Infrastructure
 
         public void Print()
         {
-            // use the CimSession to create a CimInstance object representing a WMI instance
-            // in this case, we're using the Win32_BIOS class to get information about the BIOS
-            var cimInstance = new CimInstance(@"Win32_BIOS");
-            var instance = _session.GetInstance(@"root\cimv2", cimInstance);
+            try
+            {
+                // use the CimSession to create a CimInstance object representing a WMI instance
+                // in this case, we're using the Win32_BIOS class to get information about the BIOS
+                var cimInstance = new CimInstance(@"Win32_BIOS");
+                var instance = _session.GetInstance(@"root\cimv2", cimInstance);
 
-            // print out the instance's properties
-            Console.WriteLine(
-                "BIOS Manufacturer: {0}",
-                instance.CimInstanceProperties["Manufacturer"].Value
-            );
-            Console.WriteLine("BIOS Version: {0}", instance.CimInstanceProperties["Version"].Value);
-            Console.WriteLine(
-                "BIOS Release Date: {0}",
-                instance.CimInstanceProperties["ReleaseDate"].Value
-            );
+                // print out the instance's properties
+                Console.WriteLine(
+                    "BIOS Manufacturer: {0}",
+                    instance.CimInstanceProperties["Manufacturer"].Value
+                );
+                Console.WriteLine("BIOS Version: {0}", instance.CimInstanceProperties["Version"].Value);
+                Console.WriteLine(
+                    "BIOS Release Date: {0}",
+                    instance.CimInstanceProperties["ReleaseDate"].Value
+                );
+            }
+            catch (CimException ex)
+            {
+                // handle any errors that occur when querying the remote computer
+                Console.WriteLine("An error occurred while querying the remote computer: {0}", ex.Message);
+            }
         }
     }
+
 
     public class ProcessorOutput
     {
